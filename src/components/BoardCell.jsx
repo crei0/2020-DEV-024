3b3395f1cc5d974f5fe19f43f867b302eece7fa5
A24 <->
2020-02-28 16:10:19 +0100

    Initial code for `BoardCell`

---
+++ src/components/BoardCell.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Board's cell
+ */
+class BoardCell extends React.Component {
+  render() {
+    return (
+      <div className="col-sm tic-tac-toe__board-cell">
+        board cell goes here
+      </div>
+    );
+  }
+}
+
+export default BoardCell;
--- src/components/BoardCell.test.js
+++ src/components/BoardCell.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-const BoardCell = () => {};
+import BoardCell from './BoardCell';
 
 describe('BoardCell', () => {
   it('should render a <div />', () => {
