33bae8d8c2750ff9a5911a3f1012fdfc65be0771
A24 <->
2020-02-28 16:53:05 +0100

    WIP: Test to render the correct value on `BoardCell`

--- src/components/BoardCell.test.js
+++ src/components/BoardCell.test.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import BoardCell from './BoardCell';
+import { CELL_VALUES } from '../enums/board';
 
 describe('BoardCell', () => {
   it('should render a <div />', () => {
     const wrapper = shallow(<BoardCell />);
     
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it(`Has text value of ${CELL_VALUES.CELL_X}`, () => {
+    const boardCell = shallow((
+      <BoardCell
+        y={0}
+        x={0}
+        value={CELL_VALUES.CELL_X}
+      />
+    ));
+
+    const text = boardCell.find('button').text();
+
+    expect(text).toEqual(CELL_VALUES.CELL_X);
   });
 });
