26d6dae35afcfb9aeda612acc20664c7efb0531a
A24 <->
2020-03-02 12:49:17 +0100

    Moved all the board logic to the Board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,119 @@
 import React from 'react';
 
 import { PLAYER, GAME_STATE, CELL_VALUES } from '../enums/board';
-import {
-  resetBoardState,
-  checkIfPlayerWon,
-  checkIfGameIsTiedAndNotWon,
-  getCurrentGameStateMessage
-} from '../logic/board';
 import BoardCell from '../components/BoardCell';
+
+/**
+ * Resets the grid cell values, makes player 'X' the current player, and changes the game state to be playing
+ * 
+ * @returns {Object} The re-initialized grid state
+ */
+export const resetBoardState = () => (
+  {
+    grid: [
+      [CELL_VALUES.EMPTY,  CELL_VALUES.EMPTY,   CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY,  CELL_VALUES.EMPTY,   CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY,  CELL_VALUES.EMPTY,   CELL_VALUES.EMPTY]
+    ],
+    currentPlayer: PLAYER.X,
+    gameState: GAME_STATE.PLAYING
+  }
+);
+
+/**
+ * Checks if the three cells (passed as arguments) are all the same
+ * 
+ * @param {String} cell1 The first cell to be checked
+ * @param {String} cell2 The second cell to be checked
+ * @param {String} cell3 The last cell to be checked
+ * @returns {Boolean}
+ */
+const _checkThreeCells = (cell1, cell2, cell3) => {
+  return cell1 === cell2 &&
+         cell1 === cell3 &&
+         cell1 !== CELL_VALUES.EMPTY;
+};
+
+/**
+ * Checks if a player won, 
+ * first checks by row,
+ * then by column,
+ * then diagonal top-left to bottom-right
+ * lastly diagonal bottom-left to top-right
+ * 
+ * @param {Array} grid The grid array
+ * @returns {Boolean}
+ */
+export const checkIfPlayerWon = (grid) => {
+  for (let index = 0; index < 3; index++) {
+    // Rows
+    if (_checkThreeCells(grid[index][0], grid[index][1], grid[index][2])) {
+      return true;
+    };
+
+    // Columnns
+    if (_checkThreeCells(grid[0][index], grid[1][index], grid[2][index])) {
+      return true;
+    };
+  }
+
+  // diagonal top-left to bottom-right
+  if (_checkThreeCells(grid[0][0], grid[1][1], grid[2][2])) {
+    return true;
+  }
+
+  // diagonal bottom-left to top-right
+  if (_checkThreeCells(grid[2][0], grid[1][1], grid[0][2])) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Checks if there's an empty cell
+ * 
+ * @param {Array} grid The Grid's array
+ * @returns {Boolean}
+ */
+const _checkIfThereIsStillAnEmptyCell = (grid) => {
+  // Flatten and convert to String, then check if there's an empty cell
+  return grid
+    .flat(2)
+    .toString()
+    .indexOf(CELL_VALUES.EMPTY) === -1;
+};
+
+/**
+ * Checks if the game is Tied (and not won by a player at the same time)
+ * 
+ * @param {Array} grid The Grid's array
+ * @returns {Boolean}
+ */
+export const checkIfGameIsTiedAndNotWon = (grid) => {
+  return !checkIfPlayerWon(grid) && _checkIfThereIsStillAnEmptyCell(grid);
+};
+
+/**
+ * Returns the current game state message that should appear on the top of the board,
+ * saying that the game is in Play, is Tied, or a certain player has won it
+ * 
+ * @param {(GAME_STATE|String)} gameState The current game State (GAME_STATE)
+ * @param {(PLAYER|String)} currentPlayer The currently player that is on his/her turn (PLAYER)
+ * @returns {Boolean} The game state message
+ */
+export const getCurrentGameStateMessage = (gameState, currentPlayer) => {
+  switch (gameState) {
+    case GAME_STATE.PLAYER_WON:
+      return `The player '${currentPlayer}' WON`;
+
+    case GAME_STATE.TIE:
+      return 'The game is TIED, please restart';
+
+    default:
+      return `The current player is '${currentPlayer}'`;
+  }
+};
 
 class Board extends React.Component {
   state = {
--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
 
-import Board from './Board';
+import 
+  Board,
+  {
+    resetBoardState,
+    checkIfPlayerWon,
+    checkIfGameIsTiedAndNotWon,
+    getCurrentGameStateMessage
+  } from './Board';
+import { PLAYER, GAME_STATE, CELL_VALUES } from '../enums/board';
 import BoardCell from './BoardCell';
 
 // Board is rendering correctly?
@@ -19,5 +27,186 @@ describe('Board', () => {
     const wrapper = shallow(<Board />);
 
     expect(wrapper.find(BoardCell).length).toBe(9);
+  });
+});
+
+describe('board.js > resetBoardState(...)', () => {
+  it('resetBoardState() returns an empty grid state', () => {
+    const expectedResult = {
+      grid: [
+        [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY],
+        [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY],
+        [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY]
+      ],
+      currentPlayer: PLAYER.X,
+      gameState: GAME_STATE.PLAYING
+    };
+
+    expect(resetBoardState()).toStrictEqual(expectedResult)
+  });
+});
+
+describe('board.js > checkIfPlayerWon(...)', () => {
+  // Column 1
+  it('checkIfPlayerWon(grid) detects that one player has won using the 1st column', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_O, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_O, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_X, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Column 2
+  it('checkIfPlayerWon(grid) detects that one player has won using the 2nd column', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_X, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_X, CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY, CELL_VALUES.CELL_X, CELL_VALUES.EMPTY]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Column 3
+  it('checkIfPlayerWon(grid) detects that one player has won using the 3rd column', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.CELL_X],
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.CELL_X],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Row 1
+  it('checkIfPlayerWon(grid) detects that one player has won using the 1st row', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_X, CELL_VALUES.CELL_X],
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Row 2
+  it('checkIfPlayerWon(grid) detects that one player has won using the 2nd row', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_X, CELL_VALUES.CELL_X],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Row 3
+  it('checkIfPlayerWon(grid) detects that one player has won using the 3rd row', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_X, CELL_VALUES.CELL_X]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Diagonal from the top left to the bottom right
+  it('checkIfPlayerWon(grid) detects that one player has won using the diagonal from the top left to the bottom right', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_O, CELL_VALUES.CELL_O],
+      [CELL_VALUES.EMPTY, CELL_VALUES.CELL_X, CELL_VALUES.EMPTY],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // Diagonal from the bottom left to the top right
+  it('checkIfPlayerWon(grid) detects that one player has won using the diagonal from the bottom left to the top right', () => {
+    const gridState = [
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X],
+      [CELL_VALUES.EMPTY, CELL_VALUES.CELL_X, CELL_VALUES.EMPTY],
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_O, CELL_VALUES.CELL_O]
+    ];
+    
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  // No one won
+  it('checkIfPlayerWon(grid) detects that no one has won', () => {
+    const gridState = [
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_O],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X]
+    ];
+
+    const returnedResult = checkIfPlayerWon(gridState);
+
+    expect(returnedResult).toEqual(false);
+  });
+});
+
+describe('board.js > checkIfGameIsTiedAndNotWon(...)', () => {
+  it('checkIfGameIsTiedAndNotWon(grid) detects the game is tied', () => {
+    const gridState = [
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_X, CELL_VALUES.CELL_X],
+      [CELL_VALUES.CELL_X, CELL_VALUES.CELL_X, CELL_VALUES.CELL_O],
+      [CELL_VALUES.CELL_O, CELL_VALUES.CELL_O, CELL_VALUES.CELL_X]
+    ];
+
+    const returnedResult = checkIfGameIsTiedAndNotWon(gridState);
+
+    expect(returnedResult).toEqual(true);
+  });
+
+  it('checkIfGameIsTiedAndNotWon(grid) check if game has not been won', () => {
+    const gridState = [
+      [CELL_VALUES.EMPTY, CELL_VALUES.CELL_O, CELL_VALUES.CELL_X],
+      [CELL_VALUES.EMPTY, CELL_VALUES.CELL_O, CELL_VALUES.CELL_x],
+      [CELL_VALUES.EMPTY, CELL_VALUES.EMPTY, CELL_VALUES.CELL_X]
+    ];
+
+    const returnedResult = checkIfGameIsTiedAndNotWon(gridState);
+
+    expect(returnedResult).toEqual(false);
+  });
+});
+
+describe('board.js > getCurrentGameStateMessage(...)', () => {
+  it('getCurrentGameStateMessage(GAME_STATE.PLAYER_WON, PLAYER.O) returns that the player O has won', () => {
+    const message = getCurrentGameStateMessage(GAME_STATE.PLAYER_WON, PLAYER.O);
+
+    expect(message).toEqual(`The player '${PLAYER.O}' WON`);
+  });
+
+  it('getCurrentGameStateMessage(GAME_STATE.TIE, PLAYER.X) returns that the game is tied and it needs to be restarted', () => {
+    const message = getCurrentGameStateMessage(GAME_STATE.TIE, PLAYER.X);
+
+    expect(message).toEqual('The game is TIED, please restart');
+  });
+
+  it('getCurrentGameStateMessage(GAME_STATE.PLAYING, PLAYER.X) returns that the game is being played and it is player X turn', () => {
+    const message = getCurrentGameStateMessage(GAME_STATE.PLAYING, PLAYER.X);
+
+    expect(message).toEqual(`The current player is '${PLAYER.X}'`);
   });
 });
